09009f3016cdd7f6cde8400bda788b439b1b5850
TheusStremens <matheus.beep@gmail.com>
2013-01-31 10:08:40 -0300

    Adicionado Layout de 3 telas
    Alterar saldo, cadastrar ganho e gasto

--- ControleGastos/src/com/example/controlegastos/CadastroGasto.java
+++ ControleGastos/src/com/example/controlegastos/CadastroGasto.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class CadastroGasto extends Activity {
 	@SuppressLint("NewApi")
@@ -19,6 +21,15 @@ public class CadastroGasto extends Activity {
             // Show the Up button in the action bar.
             getActionBar().setDisplayHomeAsUpEnabled(true);
         }
+		
+		Spinner spinner = (Spinner) findViewById(R.id.categorias_spinner);
+		// Create an ArrayAdapter using the string array and a default spinner layout
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+		        R.array.categoria_array, android.R.layout.simple_spinner_item);
+		// Specify the layout to use when the list of choices appears
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		// Apply the adapter to the spinner
+		spinner.setAdapter(adapter);
 	}
 
 	@Override
